d7788d2c555143ab6d36405f436cdda85e469f72
Bruno Oliveira <bruno@abstractj.org>
2012-11-05 12:24:56 -0200

    Unit testing authentication keys

---
+++ src/test/java/org/jboss/aerogear/security/picketbox/auth/AuthenticationKeyProviderTest.java
@@ -0,0 +1,93 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.aerogear.security.picketbox.auth;
+
+import org.jboss.aerogear.security.idm.AuthenticationKeyProvider;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.picketbox.cdi.PicketBoxIdentity;
+import org.picketbox.core.UserContext;
+import org.picketbox.core.session.PicketBoxSession;
+import org.picketbox.core.session.SessionId;
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.model.User;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class AuthenticationKeyProviderTest {
+
+    @Mock
+    private IdentityManager identityManager;
+
+    @Mock
+    private PicketBoxIdentity identity;
+
+    @Mock
+    private UserContext userContext;
+
+    @Mock
+    private PicketBoxSession picketBoxSession;
+
+    @Mock
+    private SessionId sessionId;
+
+    @Mock
+    private User user;
+
+    @InjectMocks
+    private AuthenticationKeyProvider keyProvider;
+
+    @Before
+    public void setUp() throws Exception {
+        keyProvider = new AuthenticationKeyProviderImpl();
+        MockitoAnnotations.initMocks(this);
+        when(sessionId.getId()).thenReturn("mySessionId");
+        when(picketBoxSession.getId()).thenReturn(sessionId);
+        when(userContext.getSession()).thenReturn(picketBoxSession);
+        when(identity.getUserContext()).thenReturn(userContext);
+    }
+
+    @Test
+    public void testGetAlreadyExistingSecret() throws Exception {
+        String secret = "32626635656566396334";
+        when(user.getAttribute("serial")).thenReturn(secret);
+        assertEquals(secret, keyProvider.getSecret());
+    }
+
+    @Test
+    public void testGetNewSecret() throws Exception {
+        assertEquals(20, keyProvider.getSecret().length());
+    }
+
+    @Test
+    public void testGetToken() throws Exception {
+        when(identity.isLoggedIn()).thenReturn(true);
+        assertEquals("mySessionId", keyProvider.getToken());
+    }
+
+    @Test
+    public void testGetBase32() throws Exception {
+        String secret = "32626635656566396334";
+        when(user.getAttribute("serial")).thenReturn(secret);
+        assertEquals(16, keyProvider.getB32().length());
+    }
+}
---
+++ src/test/java/org/jboss/aerogear/security/picketbox/auth/AuthenticationManagerTest.java
@@ -0,0 +1,58 @@
+package org.jboss.aerogear.security.picketbox.auth;
+
+import org.jboss.aerogear.security.auth.AuthenticationManager;
+import org.jboss.aerogear.security.auth.CredentialFactory;
+import org.jboss.aerogear.security.exception.AeroGearSecurityException;
+import org.jboss.aerogear.security.model.AeroGearUser;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.picketbox.cdi.PicketBoxIdentity;
+import org.picketlink.credential.LoginCredentials;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class AuthenticationManagerTest {
+
+    @Mock
+    private AeroGearUser aeroGearUser;
+    @Mock
+    private PicketBoxIdentity picketBoxIdentity;
+    @Mock
+    private CredentialFactory credentialFactory;
+
+
+    @InjectMocks
+    private AuthenticationManager authenticationManager = new AuthenticationManagerImpl();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(aeroGearUser.getId()).thenReturn("john");
+        when(aeroGearUser.getPassword()).thenReturn("123");
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        when(picketBoxIdentity.isLoggedIn()).thenReturn(true);
+        assertTrue(authenticationManager.login(aeroGearUser));
+    }
+
+    @Test(expected = AeroGearSecurityException.class)
+    public void testInvalidLogin() throws Exception {
+        when(picketBoxIdentity.isLoggedIn()).thenReturn(false);
+        authenticationManager.login(aeroGearUser);
+    }
+
+    @Test
+    public void testLogout() throws Exception {
+        when(picketBoxIdentity.isLoggedIn()).thenReturn(true);
+        authenticationManager.logout();
+        verify(picketBoxIdentity).logout();
+    }
+}
